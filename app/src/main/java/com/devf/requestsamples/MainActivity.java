20fad9863a8603e34c9152aa5b6704a0787c3202
Pedro Antonio <petterlx727@gmail.com>
2015-09-17 19:41:07 -0500

    Request with volley done

--- app/src/main/java/com/devf/requestsamples/MainActivity.java
+++ app/src/main/java/com/devf/requestsamples/MainActivity.java
@@ -2,8 +2,15 @@ package com.devf.requestsamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ejecutaPeticionConVolley("u2");
+    }
+
+    public void ejecutarPeticionConAsyncTask(){
         BusquedaArtistaAsyncTask peticion = new BusquedaArtistaAsyncTask();
         peticion.execute("Daft");
     }
 
+    public void ejecutaPeticionConVolley(String query){
+        String url =  "https://api.spotify.com/v1/search?type=artist&q=" + query;
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("Respuesta", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VolleyError", error.getMessage());
+            }
+        });
+
+
+        VolleySingleton.getInstance(this)
+                .addToRequestQueue(request);
+    }
 }
